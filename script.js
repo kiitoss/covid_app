4535e5a7a6996e3a7c72eb0bb0d0afcecf594f8d
kiitoss <antoinechataignier1998@orange.fr>
2020-03-22 19:04:13 +0100

    build: side bar with countries

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ function affiche_elements(elem) {
     showing_part.innerHTML = "";
     for (let key in elem) {
         let new_div = document.createElement("div");
+        new_div.setAttribute("class", "result")
         new_div.textContent = key+": "+elem[key];
         showing_part.appendChild(new_div);
     }
@@ -34,6 +35,30 @@ function affiche_elements(elem) {
 }
 
 function find_country() {
-    let country = document.getElementById("country").value;
+    let country = document.getElementById("country_input").value;
     resultats = renvoie_resultats("https://corona.lmao.ninja/countries/"+country);
 }
+
+
+function show_hide_nav_bar(){
+    if (nav_bar_open == false) {
+        old_class = "nav_hide";
+        new_class = "nav_show";
+    }
+    else {
+        old_class = "nav_show";
+        new_class = "nav_hide";
+    }
+
+    nav_part = document.getElementsByClassName("nav_part");
+    let i=0;
+    while (i < nav_part.length) {
+        nav_part[i].classList.remove(old_class);
+        nav_part[i].classList.add(new_class);
+        i++;
+    }
+    nav_bar_open = !nav_bar_open;
+}
+
+
+var nav_bar_open = false;
