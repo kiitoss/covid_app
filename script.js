792f3e8d7c9c7ffb5fe796cd0d173d0ef26d9a3e
kiitoss <antoinechataignier1998@orange.fr>
2020-03-22 20:12:20 +0100

    add clickable drop-down list of all countries

--- script.js
+++ script.js
@@ -34,31 +34,70 @@ function affiche_elements(elem) {
 
 }
 
-function find_country() {
-    let country = document.getElementById("country_input").value;
+function find_country(country) {
+    if (typeof(country) == 'undefined') {
+        var country = document.getElementById("country_input").value;
+    }
     resultats = renvoie_resultats("https://corona.lmao.ninja/countries/"+country);
 }
 
 
-function show_hide_nav_bar(){
+function show_hide_nav_bar(e){
+    if(e.target != document.getElementById("open_hide_director")) {
+        return;
+    }
     if (nav_bar_open == false) {
         old_class = "nav_hide";
         new_class = "nav_show";
     }
     else {
-        old_class = "nav_show";
+        old_class = "nav_show li_open";
         new_class = "nav_hide";
     }
 
     nav_part = document.getElementsByClassName("nav_part");
     let i=0;
     while (i < nav_part.length) {
-        nav_part[i].classList.remove(old_class);
+        if (nav_part[i].classList.contains(old_class) == true){
+            nav_part[i].classList.remove(old_class);
+        }
         nav_part[i].classList.add(new_class);
         i++;
     }
     nav_bar_open = !nav_bar_open;
 }
 
+function main() {
+    ajaxGet("https://corona.lmao.ninja/countries", function (reponse) {
+        var elements = JSON.parse(reponse);
+        console.log(elements);
+        let list_puces = document.getElementById("list_countries");
+        let i = 0;
+        while (i < elements.length) {
+            let puce = document.createElement("li");
+            puce.setAttribute("class", "nav_part nav_country")
+            puce.textContent = elements[i]["country"];
+            list_puces.appendChild(puce);
+            i++;
+        }
+
+        let nav_country = document.getElementsByClassName("nav_country");
+        i = 0;
+        while (i < nav_country.length) {
+            console.log(nav_country[i].innerHTML);
+            nav_country[i].addEventListener("click", function(){
+                if(nav_bar_open == false) {
+                    return;
+                }
+                find_country(this.innerHTML);
+            }, false);
+            i++;
+        }
+    });
+}
+
+window.onload=function() {
+    main();
+}
 
 var nav_bar_open = false;
