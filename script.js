df14cf81316ed9f5964471921a96528f0ee99c7f
kiitoss <antoinechataignier1998@orange.fr>
2020-03-24 20:47:52 +0100

    style: change webpage style

--- script.js
+++ script.js
@@ -3,31 +3,32 @@ function ajaxGet(url, callback) {
     req.open("GET", url);
     req.addEventListener("load", function () {
         if (req.status >= 200 && req.status < 400) {
-            // Appelle la fonction callback en lui passant la réponse de la requête
             callback(req.responseText);
         } else {
             console.error(req.status + " " + req.statusText + " " + url);
         }
     });
     req.addEventListener("error", function () {
-        console.error("Erreur réseau avec l'URL " + url);
+        console.error("Error when connect to " + url);
     });
     req.send(null);
 }
 
-function renvoie_resultats(url_send) {
-    ajaxGet(url_send, function (reponse) {
-        var elements = JSON.parse(reponse);
-        affiche_elements(elements);
+
+function return_result(url_send) {
+    ajaxGet(url_send, function (answer) {
+        let my_country = JSON.parse(answer);
+        show_statistics(my_country);
     });
 }
 
-function affiche_elements(elem) {
+
+function show_statistics(my_country) {
     if (nav_bar_open) {
         open_hide_nav();
     }
 
-    if (window.innerHeight <= reponsive_width) {
+    if (window.innerWidth <= reponsive_width) {
         document.getElementById("results").style.height = "47vh";
     }
     else {
@@ -37,21 +38,21 @@ function affiche_elements(elem) {
     let label_results = document.getElementsByClassName("label_result");
     for(let i = 0; i < label_results.length; i++) {
         label_results[i].style.visibility = "visible";
-      }
-
-    for (let key in elem) {
-        if (document.getElementById(key) != null) {
-            document.getElementById(key).innerHTML = elem[key];
-        }
     }
 
+    for (let key in my_country) {
+        if (document.getElementById(key) != null) {
+            document.getElementById(key).innerHTML = my_country[key];
+        }
+    }
 }
+
 
 function find_country(country) {
     if (typeof(country) == 'undefined') {
         var country = document.getElementById("country_input").value;
     }
-    resultats = renvoie_resultats("https://corona.lmao.ninja/countries/"+country);
+    return_result("https://corona.lmao.ninja/countries/"+country);
 }
 
 
@@ -67,11 +68,12 @@ function nav_manager(e) {
     }
 }
 
+
 function open_hide_nav() {
-    if (window.innerHeight > reponsive_width) {
+    if (window.innerWidth > reponsive_width) {
         return;
     }
-    let list_categories = document.getElementById("list_countries");
+    let nav_bar = document.getElementById("list_countries");
     let old_class, new_class;
     if (nav_bar_open) {
         old_class = "countries_show";
@@ -82,27 +84,28 @@ function open_hide_nav() {
         new_class = "countries_show";
     }
 
-    if (list_categories.classList.contains(old_class)){
-        list_categories.classList.remove(old_class);
+    if (nav_bar.classList.contains(old_class)){
+        nav_bar.classList.remove(old_class);
     }
-    list_categories.classList.add(new_class);
+    nav_bar.classList.add(new_class);
     nav_bar_open = !nav_bar_open;
 }
 
+
 function main() {
     ajaxGet("https://corona.lmao.ninja/countries", function (reponse) {
-        var elements = JSON.parse(reponse);
-        let list_puces = document.getElementById("list_countries");
+        var all_countries = JSON.parse(reponse);
+        let list_countries = document.getElementById("list_countries");
         let i = 0;
-        while (i < elements.length) {
-            let puce = document.createElement("li");
-            puce.className = "country";
-            let info_puce = "<span class='info_country country_part'>"+elements[i]["country"]+"</span><span class='info_cases country_part'>"+elements[i]["cases"]+"</span><span class='info_deaths country_part'>"+elements[i]["deaths"]+"</span><span class='info_recovered country_part'>"+elements[i]["recovered"]+"</span>";
-            puce.innerHTML =  info_puce;
-            list_puces.appendChild(puce);
-            for (let key in elements[i]) {
+        while (i < all_countries.length) {
+            let new_country = document.createElement("li");
+            new_country.className = "country";
+            let info_puce = "<span class='info_country country_part'>"+all_countries[i]["country"]+"</span><span class='info_cases country_part'>"+all_countries[i]["cases"]+"</span><span class='info_deaths country_part'>"+all_countries[i]["deaths"]+"</span><span class='info_recovered country_part'>"+all_countries[i]["recovered"]+"</span>";
+            new_country.innerHTML =  info_puce;
+            list_countries.appendChild(new_country);
+            for (let key in all_countries[i]) {
                 if (total[key] != null) {
-                    total[key] += elements[i][key];
+                    total[key] += all_countries[i][key];
                 }
             }
             i++;
@@ -116,11 +119,21 @@ function main() {
     });
 }
 
+
 window.onload=function() {
     main();
 }
 
+
 var nav_bar_open = false;
-var total_stats = [0, 0, 0, 0, 0, 0, 0, 0];
-var total = {"cases": 0, "recovered": 0, "deaths": 0, "active": 0, "critical": 0, "todayDeaths": 0, "todayCases": 0, "casesPerOneMillion": 0};
+var total = {
+    "cases": 0,
+    "recovered": 0,
+    "deaths": 0,
+    "active": 0,
+    "critical": 0,
+    "todayDeaths": 0,
+    "todayCases": 0,
+    "casesPerOneMillion": 0
+};
 var reponsive_width = 850;
